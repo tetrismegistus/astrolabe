e668c91a5e0104fa3d2f6c23da678707e080e579
Maddux <AMaddux@citizensenergygroup.com>
2017-05-30 11:17:58 -0400

    removed page numbers from journalpages in pagetemplate

--- printjournal.py
+++ printjournal.py
@@ -2,8 +2,8 @@ from datetime import date, datetime, timedelta
 import astrolabe
 import journalpage
 
-month = 6
-datetime_obj = datetime(datetime.now().year, month, 1, 12)
+month = 5
+datetime_obj = datetime(datetime.now().year, month, 30, 12)
 DAY = astrolabe.Day(datetime_object=datetime_obj)
 journal = ''
 while datetime_obj.month == month:
@@ -12,4 +12,4 @@ while datetime_obj.month == month:
     DAY.set_date(datetime_object=datetime_obj)
 
 
-journalpage.write_journal(journal, output_file='june.tex')
+journalpage.write_journal(journal, output_file='may.tex')
